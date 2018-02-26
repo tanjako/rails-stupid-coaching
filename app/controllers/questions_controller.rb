26a9c9f601bccba704fc15d88fa14d8f7b734dfd
Tanja Kohlen <tanja.kohlen@gmail.com>
2018-02-26 15:45:03 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+    # render questions/answer
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
+
